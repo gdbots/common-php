18d28003f0ae6dea952ba4122ad0e31e2a8dbc3d
Greg Brown <greg@12dnetworks.com>
2015-01-19 10:47:12 -0800

    add number utils

---
+++ src/Gdbots/Common/Util/NumberUtils.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Gdbots\Common\Util;
+
+class NumberUtils
+{
+    /**
+     * Returns an integer within a boundary.
+     *
+     * @param int $number
+     * @param int $min
+     * @param int $max
+     * @return int
+     */
+    public static function bound($number, $min = 0, $max = PHP_INT_MAX)
+    {
+        $number = (int) $number;
+        $min = (int) $min;
+        $max = (int) $max;
+
+        return min(max($number, $min), $max);
+    }
+}
